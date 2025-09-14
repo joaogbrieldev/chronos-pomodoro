01832c08a4f71d3bef5378eec388cfda66673a20
João Figueiredo <joao.ggwp@outlook.com>
2025-09-14 14:30:00 -0300

    style: create initial layout css for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Heading } from './components/heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>Teste</Heading>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
