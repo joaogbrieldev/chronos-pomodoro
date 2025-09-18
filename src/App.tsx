3496d966422917346f44570d8effd162c3f7da66
João Figueiredo <joao.ggwp@outlook.com>
2025-09-17 21:56:07 -0300

    feat: create Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
+import { Container } from './components/Container';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
+      <Container>
+        <section>FORM</section>
+      </Container>
+      <Container>
+        <section>FOOTER</section>
+      </Container>
+      <Container>
+        <section>FOOTER</section>
+      </Container>
     </>
   );
 }
