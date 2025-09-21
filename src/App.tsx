56c2bb8eb43236473b1210f1d986b54120335aa2
João Figueiredo <joao.ggwp@outlook.com>
2025-09-21 12:43:48 -0300

    feat: create initial structure to form

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,24 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input type='text' id='meuInput' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
