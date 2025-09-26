2b32f91893f5776291709175301825a2086ed5a4
João Figueiredo <joao.ggwp@outlook.com>
2025-09-25 21:31:54 -0300

    feat: add react hooks to change theme

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,20 +1,62 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storedTheme = localStorage.getItem('theme');
+    return (storedTheme as AvailableThemes) || 'dark';
+  });
+  function toggleDarkMode(ev: React.MouseEvent<HTMLAnchorElement>) {
+    ev.preventDefault();
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='/'>
+      <a
+        className={styles.menuLink}
+        href='/'
+        aria-label='Ir para a home'
+        title='Ir para a home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='/'>
+      <a
+        className={styles.menuLink}
+        href='/'
+        aria-label='Ir para o histórico'
+        title='Ir para o histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='/'>
+      <a
+        className={styles.menuLink}
+        href='/'
+        aria-label='Ir para as configurações'
+        title='Ir para as configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='/'>
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href='#'
+        onClick={toggleDarkMode}
+        aria-label='Alternar entre o modo claro e escuro'
+        title='Alternar entre o modo claro e escuro'
+      >
+        {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
       </a>
     </nav>
   );
